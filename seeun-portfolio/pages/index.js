4e7879dd75b31ca598d1839cba26d004aa65fe3f
kimseeun <cocobell3@naver.com>
2024-01-18 22:44:32 +0900

    feat: layout만들기

---
+++ seeun-portfolio/pages/index.js
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+import Layout from "../components/layout";
+// import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <Layout>
+      {/* <Head> */}
+      <title>김세은 포트폴리오</title>
+      <meta name="description" content="오늘도 최선을!" />
+      <link rel="icon" href="/favicon.ico" />
+      {/* </Head> */}
+      <h1>홈입니다</h1>
+    </Layout>
+  );
+}
