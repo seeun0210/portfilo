4e678b0ef45743a6f745c4b3d596ec6e3a91dbfe
kimseeun <cocobell3@naver.com>
2024-01-21 18:05:46 +0900

    feat: 이미지 넣음

--- seeun-portfolio/pages/index.js
+++ seeun-portfolio/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 import Layout from "../components/layout";
-
+import Hero from "@/components/home/hero";
 export default function Home() {
   return (
     <Layout>
@@ -11,25 +11,7 @@ export default function Home() {
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              안녕하세요. 서버 개발자 김세은입니다.
-              <br className="hidden lg:inline-block" />
-              책임감을 갖고 열심히 공부하며 위기에도 차분하게 문제를 해결합니다.
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                프로젝트 보러가기
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
+          <Hero />
         </div>
       </section>
     </Layout>
