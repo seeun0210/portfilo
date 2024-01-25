9c022e60284af5d065f52199a3c57def2b894959
kimseeun <cocobell3@naver.com>
2024-01-25 21:24:26 +0900

    feat:   연락하기 페이지 기능 구현

---
+++ seeun-portfolio/pages/api/contact.js
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+import { AUTH_PASS, AUTH_USER } from "@/config";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: AUTH_USER,
+    pass: AUTH_PASS,
+  },
+});
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  const body = req.body;
+
+  try {
+    await sendEmail(body);
+    return res.status(200).json({ message: "메일을 성공적으로 보냈습니다." });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: `메일 전송에 실패하였습니다.${error}` });
+  }
+}
+
+export async function sendEmail({ name, email, message }) {
+  const mailData = {
+    to: AUTH_USER,
+    subject: `[portfolio]sender:${name}`,
+    from: email,
+    html: `<p>${message}</p>`,
+  };
+
+  try {
+    await transporter.sendMail(mailData);
+  } catch (error) {
+    console.error(error);
+    throw new Error(`메일 전송에 실패하였습니다.${error}`);
+  }
+}
