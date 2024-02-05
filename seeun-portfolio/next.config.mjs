aa2ee02e2c36929e18f8b826b39058ec6385f48f
kimseeun <cocobell3@naver.com>
2024-02-05 23:29:57 +0900

    feat: 노션 s3이미지 접근 불가

--- seeun-portfolio/next.config.mjs
+++ seeun-portfolio/next.config.mjs
@@ -26,7 +26,6 @@ const nextConfig = {
     domains: [
       "wwww.notion.so",
       "images.unsplash.com",
-      "prod-files-secure.s3.us-west-2.amazonaws.com",
       "noticon-static.tammolo.com",
       "i.ibb.co",
     ],
